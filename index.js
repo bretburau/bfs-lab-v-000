77da4c6463553f8794b979a671364f95cef7ecb1
Bret <Burhooo@gmail.com>
2018-09-27 12:17:50 -0500

    passing 1st test

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-function bfs(rootNode, vertices, edges){
 
+function bfs(rootNode, vertices, edges){
+ 
+}
+
+function findAdjacentNodes(node, vertices, edges) {
+    let thisNode = vertices[node]
+    let adjacents = []
+    edges.map(edge => {
+        if(edge[0] === node) {
+          adjacents.push(vertices.find((e) => {return e.name === edge[1]}))
+        } else if(edge[1] === node) {
+          adjacents.push(vertices.find((e) => {return e.name === edge[0]}))
+        }
+    })
+  return adjacents;
 }
